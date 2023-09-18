4ad15f28a094dd484e0fca6048624627d088e3ed
dearian <dearian2706@gmail.com>
2023-09-18 23:55:03 +0300

    starting server not working

--- launch.js
+++ launch.js
@@ -1,24 +1,47 @@
-const execSync = require('child_process').execSync;
+const { exec, execSync } = require('child_process');
 const readline = require('readline');
+const spinners = require('cli-spinners');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-function checkAndInstallDependencies() {
+async function checkAndInstallDependencies() {
+
   try {
-    exec('npm ls express --depth=0', { stdio: 'ignore' });
+    execSync('npm ls express --depth=0');
   } catch (error) {
     console.error('Express не встановлено. Встановлюємо...');
-    exec('npm install express --save');
+    execSync('npm install express --save');
   } finally {
+    // clearInterval(loadingInterval);
     console.log('Express встановлено');
+    console.log('Завантаження сервера...');
+
+    const server = exec('npm start', (error, stdout, stderr) => {
+      if (error) {
+        console.error('Помилка завантаження сервера: ', error);
+      }
+    });
+
+    server.stdout.on('data', (data) => {
+      if (data.includes('Server started')) {
+        rl.question('Press Enter to exit...', () => {
+          server.kill(); // Stop the server process
+          rl.close();
+        });
+      }
+    });
   }
+  // try {
+  //   exec('npm start');
+  // } catch (error) {
+  //   console.error('Помилка завантаження сервера: ', error);
+  // }
 }
 
 checkAndInstallDependencies();
-
 
 rl.question('Press Enter to exit...', () => {
   rl.close();
